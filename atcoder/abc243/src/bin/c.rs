825c9fb7221b492ccb3acd4fe14d309cca5b338e
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-03 23:28:48 +0900

    abc243 c

--- atcoder/abc243/src/bin/c.rs
+++ atcoder/abc243/src/bin/c.rs
@@ -1,3 +1,28 @@
+use std::{collections::HashMap, process};
+use std::cmp;
+
+use proconio::{input, marker::Chars};
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        XY: [[usize; 2]; N],
+        S: Chars,
+    }
+    let mut hash: HashMap<usize, (usize, usize)> = HashMap::new();
+    for i in 0..N {
+        hash.entry(XY[i][1]).or_insert((1_000_000_001, 0));
+        let tuple = hash.get(&XY[i][1]).unwrap();
+        if S[i] == 'L' {
+            hash.insert(XY[i][1], (tuple.0, cmp::max(tuple.1, XY[i][0])));
+        } else {
+            hash.insert(XY[i][1], (cmp::min(tuple.0, XY[i][0]), tuple.1));
+        }
+    }
+    for (k, tuple) in hash.iter() {
+        if tuple.0 < tuple.1 {
+            println!("Yes");
+            process::exit(0);
+        }
+    }
+    println!("No");
 }
