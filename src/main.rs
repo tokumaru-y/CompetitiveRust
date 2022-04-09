1c9cf8c17eda0532314c5a5a355247cbd0cbddbb
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-10 02:27:27 +0900

    abc245 TLE

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
-use proconio::input;
+use std::{collections::BinaryHeap, cmp::Reverse};
 
-fn calc(a: &usize, b: &usize) -> usize{
-    a*a*a + (a*a)*b + a*(b*b) + b*b*b
-}
-
+use proconio::{input, marker::Chars};
 fn main() {
     input!{
         N: usize,
+        mut sx: usize,
+        mut sy: usize,
+        mut ex: usize,
+        mut ey: usize,
+        Grid: [Chars; N],
     }
-    let mut ans = 1_000_000_000_000_000_000;
-    for a in 0..1000_001{
-        let mut left = 0;
-        let mut right = 1000_001;
-        while right - left > 1 {
-            let middle = (right+left) / 2;
-            if calc(&a, &middle) >= N {
-                right = middle;
-            } else {
-                left = middle;
+    let mut dist = vec![vec![100_000_000_usize; N]; N];
+    sx -= 1;sy -= 1; ex -=1; ey -=1;
+    dist[sx][sy] = 0;
+    let move_direction: [[isize; 2]; 4] = [[-1,-1],[1,1],[1,-1],[-1,1]];
+    let mut heap = BinaryHeap::new();
+    heap.push((0 as isize, sx, sy));
+    while heap.len() > 0 {
+        let tmp_v = heap.pop().unwrap();
+        let now_times = (tmp_v.0 * -1) as usize;let nx = tmp_v.1;let ny = tmp_v.2;
+        for mv in &move_direction {
+            let mx = &mv[0];let my = &mv[1];
+            if mx + (nx as isize) < 0 || my + (ny as isize) < 0 {continue;};
+            let mut x = (mx + nx as isize) as usize;
+            let mut y = (my + ny as isize) as usize;
+            while x <= N-1 && y <= N-1 {
+                if Grid[x][y] == '#'{ break;};
+                if 1 + now_times >= dist[x][y] {
+                    x = (x as isize + mx ) as usize;
+                    y = (y as isize + my ) as usize;
+                    continue;
+                };
+                dist[x][y] = 1 + now_times;
+                let t = 1 + now_times;
+                heap.push((-1 * t as isize, x, y));
+                if x as isize + mx < 0 || y as isize + my < 0 {
+                    break;
+                } else {
+                    x = (x as isize + mx ) as usize;
+                    y = (y as isize + my ) as usize;
+                }
             }
         }
-        let cmp_num = if calc(&a, &left) >= N { left } else { right };
-        ans = std::cmp::min(ans, calc(&a, &cmp_num));
     }
+    let ans: isize = if dist[ex][ey] == 100_000_000_usize { -1 } else { dist[ex][ey] as isize };
     println!("{}", ans);
 }
