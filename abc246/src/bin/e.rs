7ebd4370156e53fb72fe53195296ccd2323726c7
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-10 15:54:04 +0900

    abc246 e

--- abc246/src/bin/e.rs
+++ abc246/src/bin/e.rs
@@ -1,5 +1,61 @@
-use proconio::input;
+use std::collections::VecDeque;
 
+use proconio::{input, marker::Chars};
+struct P {
+    x: usize,
+    y: usize,
+    dir: usize,
+}
 fn main() {
-    todo!();
+    input!{
+        N: usize,
+        mut sx: usize,
+        mut sy: usize,
+        mut ex: usize,
+        mut ey: usize,
+        Grid: [Chars; N],
+    }
+    let mut passed = vec![vec![vec![100_000_000_usize; 4];N];N];
+    sx-=1;sy-=1;ex-=1;ey-=1;
+    for i in 0..4{passed[sx][sy][i]=0;}
+    let move_directions = vec![[-1,-1],[-1,1],[1,1],[1,-1]];
+    let mut deque = VecDeque::new();
+    for i in 0..4{
+        if (sx as isize + move_directions[i][0]) < 0 || (sy as isize + move_directions[i][1]) < 0 {continue;};
+        let x = (sx as isize + move_directions[i][0]) as usize;
+        let y = (sy as isize + move_directions[i][1]) as usize;
+        if x >= N || y >= N || Grid[x][y] == '#'{continue;};
+        deque.push_front(P{x,y,dir:i});
+        passed[x][y][i] = 1;
+    }
+    while !deque.is_empty() {
+        let tmp_p = deque.pop_front().unwrap();
+        if tmp_p.x == ex && tmp_p.y == ey {
+            println!("{}", passed[tmp_p.x][tmp_p.y][tmp_p.dir]);
+            std::process::exit(0);
+        }
+        for i in 0..4 {
+            let mut nx = tmp_p.x as isize + move_directions[i][0];
+            let mut ny = tmp_p.y as isize + move_directions[i][1];
+            
+            if !(0<= nx) || !(0<= ny) {continue;};
+            let nx = nx as usize;let ny = ny as usize;
+            if !(nx <= N-1) || !(ny <= N-1) {continue;};
+            if Grid[nx][ny] == '#' {continue;};
+
+            let mut cnt = passed[tmp_p.x][tmp_p.y][tmp_p.dir];
+            if tmp_p.dir != i { cnt += 1};
+
+            if passed[nx][ny][i] > cnt {
+                passed[nx][ny][i] = cnt;
+                if tmp_p.dir == i {
+                    deque.push_front(P{x:nx,y:ny,dir:i});
+                } else {
+                    deque.push_back(P{x:nx,y:ny,dir:i});
+                }
+            }
+
+        }
+    }
+    println!("-1");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,4 @@ use std::{collections::BinaryHeap, cmp::Reverse};
 
 use proconio::{input, marker::Chars};
 fn main() {
-    input!{
-        N: usize,
-        mut sx: usize,
-        mut sy: usize,
-        mut ex: usize,
-        mut ey: usize,
-        Grid: [Chars; N],
-    }
-    let mut dist = vec![vec![100_000_000_usize; N]; N];
-    sx -= 1;sy -= 1; ex -=1; ey -=1;
-    dist[sx][sy] = 0;
-    let move_direction: [[isize; 2]; 4] = [[-1,-1],[1,1],[1,-1],[-1,1]];
-    let mut heap = BinaryHeap::new();
-    heap.push((0 as isize, sx, sy));
-    while heap.len() > 0 {
-        let tmp_v = heap.pop().unwrap();
-        let now_times = (tmp_v.0 * -1) as usize;let nx = tmp_v.1;let ny = tmp_v.2;
-        for mv in &move_direction {
-            let mx = &mv[0];let my = &mv[1];
-            if mx + (nx as isize) < 0 || my + (ny as isize) < 0 {continue;};
-            let mut x = (mx + nx as isize) as usize;
-            let mut y = (my + ny as isize) as usize;
-            while x <= N-1 && y <= N-1 {
-                if Grid[x][y] == '#'{ break;};
-                if 1 + now_times >= dist[x][y] {
-                    x = (x as isize + mx ) as usize;
-                    y = (y as isize + my ) as usize;
-                    continue;
-                };
-                dist[x][y] = 1 + now_times;
-                let t = 1 + now_times;
-                heap.push((-1 * t as isize, x, y));
-                if x as isize + mx < 0 || y as isize + my < 0 {
-                    break;
-                } else {
-                    x = (x as isize + mx ) as usize;
-                    y = (y as isize + my ) as usize;
-                }
-            }
-        }
-    }
-    let ans: isize = if dist[ex][ey] == 100_000_000_usize { -1 } else { dist[ex][ey] as isize };
-    println!("{}", ans);
 }
