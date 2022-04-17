2142281fda9c2a68b0ba380ff0e44a14a64f2432
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-18 00:43:09 +0900

    リファクタリング

--- abc247/src/bin/d.rs
+++ abc247/src/bin/d.rs
@@ -1,14 +1,32 @@
 use std::{collections::{BinaryHeap, HashMap, VecDeque}, cmp::Reverse};
 
 use proconio::{input, marker::Chars};
+
+fn operation2(&mut deque: VecDeque<Vec<usize,usize>>, &mut ans: Vec<usize>){
+    input!{
+        mut c: usize,
+    }
+    let mut tmp_ans = 0;
+    while c > 0 {
+        let v = deque.pop_front().unwrap();
+        if v[1] > c {
+            v[1] -= c;
+            tmp_ans += c * v[0];
+            v.push_front(v);
+        } else {
+            tmp_ans += v[1] * v[0];
+            c -= v[1];
+        }
+    }
+    ans.append(tmp_ans);
+}
+
 fn main() {
     input!{
         Q: usize,
     }
     let mut ans = Vec::new();
     let mut deque = VecDeque::new();
-    let mut pre_cnt = 0;
-    let mut pre_num = 0;
     for _ in 0..Q{
         input!{
             cmd: usize,
@@ -20,37 +38,7 @@ fn main() {
             }
             deque.push_back([x,c]);
         } else {
-            input!{
-                mut c: usize,
-            }
-            let mut tmp = 0;
-            if pre_cnt < c {
-                tmp = pre_num * pre_cnt;
-                c -= pre_cnt;
-                pre_cnt = 0;
-                pre_num = 0;
-                while c > 0 {
-                    let num_cnt = deque.pop_front().unwrap();
-                    let num = num_cnt[0];
-                    let cnt = num_cnt[1];
-                    if c >= cnt {
-                        c -= cnt;
-                        tmp += cnt * num;
-                    } else {
-                        pre_cnt = cnt - c;
-                        pre_num = num;
-                        tmp += c * num;
-                        c=0;
-                    }
-                }
-                if tmp >0 {
-                    ans.push(tmp);
-                }
-            } else {
-                ans.push(c * pre_num);
-                pre_cnt -= c;
-                c = 0;
-            }
+            operation2(&mut deque, &mut ans);
         }
     }
     println!("{}", ans.iter().map(|x| x.to_string()).collect::<Vec<String>>().join("\n"));
