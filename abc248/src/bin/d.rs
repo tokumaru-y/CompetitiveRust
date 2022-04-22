bb2888454490c4c644531ed85ff4c8740b3413da
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-22 23:36:04 +0900

    abc248 d,e

--- abc248/src/bin/d.rs
+++ abc248/src/bin/d.rs
@@ -1,5 +1,31 @@
-use proconio::input;
+use std::{collections::{BinaryHeap, HashMap, VecDeque}, cmp::Reverse};
+
+use proconio::{input, marker::Chars};
 
 fn main() {
-    todo!();
-}
+    input!{
+        N: usize,
+        A: [usize; N],
+        Q: usize,
+        queries: [[usize; 3]; Q],
+    }
+    let mut number_list = vec![vec![]; 2 * 10_0001];
+    for i in 0..N {
+        number_list[A[i]].push(i);
+    }
+    for iter in queries.iter() {
+        let left = iter[0] - 1;
+        let right = iter[1] - 1;
+        let target = iter[2];
+        
+        let left_ind = match number_list[target].binary_search(&left) {
+            Ok(x) => x+1,
+            Err(x) => x+1,
+        };
+        let right_ind = match number_list[target].binary_search(&right) {
+            Ok(x) => x+2,
+            Err(x) => x+1,
+        };
+        println!("{}", right_ind - left_ind);
+    }
+}   
--- abc248/src/bin/e.rs
+++ abc248/src/bin/e.rs
@@ -1,5 +1,38 @@
-use proconio::input;
+use std::{collections::{BinaryHeap, HashMap, VecDeque}, cmp::Reverse};
+
+use proconio::{input, marker::Chars};
 
 fn main() {
-    todo!();
-}
+    input! {
+        N: usize,
+        K: usize,
+        XY: [[isize; 2]; N],
+    }
+    if K == 1 {
+        println!("Infinity");
+        std::process::exit(0);
+    }
+    let mut hash = HashMap::<isize, usize>::new();
+    let mut tilt = Vec::new();
+    for i in 0..N {
+        for j in i+1..N{
+            let a = &XY[i];
+            let b = &XY[j];
+            let x = b[0] - a[0];
+            let y = b[1] - a[1];
+            tilt.push((x,y));
+        }
+    }
+    for i in 0..tilt.len() {
+        for j in i+1..tilt.len() {
+            let mut cnt = hash.entry(tilt[i].1 * tilt[j].0).or_insert(1);
+            *cnt += 1;
+        }
+    }
+    let mut ans = 0;
+    println!("{:?}", hash);
+    for (k, v) in hash.iter() {
+        if *v >= K { ans += 1 ;}
+    }
+    println!("{}", ans);
+}   
