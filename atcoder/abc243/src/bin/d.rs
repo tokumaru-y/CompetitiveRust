a6c27905a35ab88b51b4e2162bac66908a1611c8
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-24 01:05:25 +0900

    more

--- atcoder/abc243/src/bin/d.rs
+++ atcoder/abc243/src/bin/d.rs
@@ -1,3 +1,40 @@
+use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse};
+
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        mut X: usize,
+        S: Chars,
+    }
+    let mut s = VecDeque::new();
+    let mut skip_cnt = 0;
+    for i in (0..N).rev() {
+        match S[i] {
+            'U' => {
+                skip_cnt += 1;
+            }
+            _ => {
+                if skip_cnt > 0 {
+                    skip_cnt -=1;
+                } else {
+                    s.push_front(S[i]);
+                }
+            }
+        }
+    }
+    while skip_cnt > 0 {
+        X /= 2;
+        skip_cnt -=1;
+    }
+    for c in s.iter() {
+        if *c == 'L' {
+            X *= 2;
+        } else {
+            X *= 2;
+            X += 1;
+        }
+    }
+    println!("{}", X);
 }
