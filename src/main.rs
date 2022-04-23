ab39b1b235fc228ce14103938687c41565c4e54a
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-24 01:05:02 +0900

    abc243d

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,36 @@ use proconio::{input, marker::Chars};
 fn main() {
     input! {
         N: usize,
-        M: usize,
-        K: usize,
-        S: usize,
-        T: usize,
-        X: usize,
-        UV: [[usize; 2]; M],
+        mut X: usize,
+        S: Chars,
     }
-    let MOD = 998244353;
-    let mut list = vec![Vec::new(); N];
-    for i in 0..M {
-        let n = &UV[i];
-        let a = n[0] - 1 ;let b = n[1] - 1;
-        list[a].push(b);
-        list[b].push(a);
-    }
-    let mut dp = vec![vec![vec![0; 2]; N]; K+1];
-    if S == X {
-        dp[0][S-1][1] = 1;
-    } else {
-        dp[0][S-1][0] = 1;
-    }
-    for k in 0..K{
-        for j in 0..N {
-            for i in 0..2 {
-                for l_ind in 0..list[j].len() {
-                    let n_ind = list[j][l_ind];
-                    if n_ind + 1 == X {
-                        dp[k+1][n_ind][i] += dp[k][j][(i+1)%2];
-                    } else {
-                        dp[k+1][n_ind][i] += dp[k][j][i];
-                    }
-                    dp[k+1][n_ind][i] %= MOD;
+    let mut s = VecDeque::new();
+    let mut skip_cnt = 0;
+    for i in (0..N).rev() {
+        match S[i] {
+            'U' => {
+                skip_cnt += 1;
+            }
+            _ => {
+                if skip_cnt > 0 {
+                    skip_cnt -=1;
+                } else {
+                    s.push_front(S[i]);
                 }
             }
         }
     }
-    println!("{}", dp[K][T-1][0] % MOD);
+    while skip_cnt > 0 {
+        X /= 2;
+        skip_cnt -=1;
+    }
+    for c in s.iter() {
+        if *c == 'L' {
+            X *= 2;
+        } else {
+            X *= 2;
+            X += 1;
+        }
+    }
+    println!("{}", X);
 }
