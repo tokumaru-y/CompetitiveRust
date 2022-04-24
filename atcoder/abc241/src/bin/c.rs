66d9c1a736589684512ffd0c05be882fcf4fe214
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-25 01:33:51 +0900

    abc241c

--- atcoder/abc241/src/bin/c.rs
+++ atcoder/abc241/src/bin/c.rs
@@ -1,3 +1,92 @@
+use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse};
+
+use proconio::{input, marker::Chars};
+
+
+fn check_h(grid: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+    let len = grid.len();
+    let mut used_cnt = 0;
+    for h in 0..6 {
+        if i+h >= len {
+            return false;
+        }
+        if grid[h+i][j] == '.' {
+            if used_cnt >= 2 {
+                return false;
+            }
+            used_cnt+=1;
+        }
+    }
+    true
+}
+
+fn check_w(grid: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+    let len = grid.len();
+    let mut used_cnt = 0;
+    for w in 0..6 {
+        if j+w >= len {
+            return false;
+        }
+        if grid[i][j+w] == '.' {
+            if used_cnt >= 2 {
+                return false;
+            }
+            used_cnt+=1;
+        }
+    }
+    true
+}
+
+fn check_hw(grid: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+    let len = grid.len();
+    let mut used_cnt = 0;
+    for k in 0..6 {
+        if i+k >= len || j+k >= len {
+            return false;
+        }
+        if grid[i+k][j+k] == '.' {
+            if used_cnt >= 2 {
+                return false;
+            }
+            used_cnt+=1;
+        }
+    }
+    true
+}
+
+fn check_wh(grid: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+    let len = grid.len();
+    let mut used_cnt = 0;
+    for k in 0..6 {
+        if i+k >= len || j < k {
+            return false;
+        }
+        if grid[i+k][j-k] == '.' {
+            if used_cnt >= 2 {
+                return false;
+            }
+            used_cnt+=1;
+        }
+    }
+    true
+}
+
+fn is_acceptable(grid: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+    check_h(grid, i, j) || check_w(grid, i, j) || check_hw(grid, i, j) || check_wh(grid, i, j)
+}
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        Grid: [Chars; N],
+    }
+    for i in 0..N{
+        for j in 0..N {
+            if is_acceptable(&Grid, i, j) {
+                println!("Yes");
+                std::process::exit(0);
+            }
+        }
+    }
+    println!("No");
 }
--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,10 @@ use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        mut T: usize,
+        mut Q: usize,
     }
-    let MOD = 998244353;
-    let mut alpha_tabel = HashMap::new();
-    for (i, b) in (b'A'..=b'Z').enumerate() {
-        alpha_tabel.insert(char::from(b), i);
-    }
-    while T > 0 {
-        T -= 1;
-        input! {
-            N: usize,
-            S: Chars,
-        }
-        let mut ans = 0;
-        let limit = (N-1)/2;
-        for i in 0..=limit {
-            ans *= 26;
-            ans %= MOD;
-            ans += alpha_tabel[&S[i]];
-            ans %= MOD;
-        }
-        let target = S.iter().collect::<String>();
-        let mut rev_target = S.to_vec();
-        let mut i = 0; let mut j = N-1;
-        while i<j {
-            rev_target[j] = rev_target[i];
-            i+=1;j-=1;
-        }
-        let rev_target = rev_target.iter().collect::<String>();
-        if target >= rev_target {
-            ans += 1;
-            ans %= MOD;
-        }
-        println!("{}",ans);
+    while Q > 0 {
+        Q -= 1;
+        
     }
 }
