d23d734f90a350eee49b748262d728851c67eb70
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-27 00:55:25 +0900

    abc240c-d

--- atcoder/abc240/src/bin/c.rs
+++ atcoder/abc240/src/bin/c.rs
@@ -1,3 +1,22 @@
+use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse};
+use std::ops::Bound::Included;
+use proconio::{input, marker::Chars};
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        X: usize,
+        AB: [[usize; 2]; N]
+    }
+    let mut dp = vec![vec![false; 10001]; 101];
+    dp[0][0] = true;
+    for i in 0..N {
+        for j in 0..=10000 {
+            let next = &AB[i];
+            if (j >= next[0] && (dp[i][j-next[0]])) || (j >= next[1] && (dp[i][j-next[1]])) {
+                dp[i+1][j] = true;
+            }
+        }
+    }
+    let ans = if dp[N][X] { "Yes" } else { "No"};
+    println!("{}",ans);
 }
--- atcoder/abc240/src/bin/d.rs
+++ atcoder/abc240/src/bin/d.rs
@@ -1,3 +1,30 @@
+use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse};
+use std::ops::Bound::Included;
+use proconio::{input, marker::Chars};
 fn main() {
-    todo!();
+    input!{
+        N: usize,
+        A: [usize; N],
+    }
+    let mut deque = VecDeque::new();
+    let mut ans = 0;
+    let mut pre_num = 2*10_0001;
+    let mut pre_cnt = 0;
+    for i in 0..N{
+        ans += 1;
+        if pre_num == A[i] {
+            pre_cnt += 1;
+        } else {
+            deque.push_back((pre_num, pre_cnt));
+            pre_num = A[i];
+            pre_cnt = 1;
+        }
+        if pre_cnt == pre_num {
+            ans -= pre_cnt;
+            let next = deque.pop_back().unwrap();
+            pre_num = next.0;
+            pre_cnt = next.1;
+        }
+        println!("{}", ans);
+    }
 }
