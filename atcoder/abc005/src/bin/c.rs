c52a03cd69f385c39439b41884809f2320642487
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-04-29 19:27:06 +0900

    2-2-4 done

--- atcoder/abc005/src/bin/c.rs
+++ atcoder/abc005/src/bin/c.rs
@@ -1,5 +1,37 @@
-use proconio::input;
-
+#[allow(unused_imports)]
+use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse};
+#[allow(unused_imports)]
+use proconio::{input, marker::Chars};
 fn main() {
-    todo!();
+    input! {
+        T: usize,
+        N: usize,
+        A: [usize; N],
+        M: usize,
+        B: [usize; M],
+    }
+    let mut deque = VecDeque::new();
+    let mut ab_vec = Vec::new();
+    for a in A.iter() {
+        ab_vec.push((*a, 1))
+    }
+    for b in B.iter() {
+        ab_vec.push((*b, 2));
+    }
+    ab_vec.sort();
+    for ab in ab_vec.into_iter() {
+        if ab.1 == 1 {
+            deque.push_back((ab.0 + T));
+        } else {
+            while true {
+                let time = deque.pop_front();
+                if time.is_none() {
+                    println!("no");
+                    std::process::exit(0);
+                }
+                if ab.0 <= time.unwrap() { break; }
+            }
+        }
+    }
+    println!("yes");
 }
--- atcoder/arc006/src/bin/c.rs
+++ atcoder/arc006/src/bin/c.rs
@@ -1,5 +1,17 @@
-use proconio::input;
-
+#[allow(unused_imports)]
+use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse};
+#[allow(unused_imports)]
+use proconio::{input, marker::Chars};
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        W: [usize; N],
+    }
+    let mut list = vec![100_001; N];
+    for w in W.iter() {
+        let index = list.binary_search(w).unwrap_or_else(|x| x);
+        list[index] = *w;
+    }
+    let mut ans = list.iter().filter(|x| **x < 100_001).count();
+    println!("{}", ans);
 }
