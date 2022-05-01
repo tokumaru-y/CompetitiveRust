7ffaebd951f6de047338f7af33eaec81eccca3f8
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-05-01 20:08:16 +0900

    abc238 e

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,56 @@
 use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse, cmp::{max, min}};
 #[allow(unused_imports)]
 use proconio::input;
+// reference: https://github.com/rust-lang-ja/ac-library-rs/blob/master/src/dsu.rs
+struct UnionFind {
+    node: usize,
+    parent_or_size: Vec<isize>,
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> Self {
+        Self {
+            node: size,
+            parent_or_size: vec![-1; size]
+        }
+    }
+
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    pub fn root(&mut self, x: usize) -> usize {
+        if self.parent_or_size[x] < 0 {
+            return x;
+        }
+        self.parent_or_size[x] = self.root(self.parent_or_size[x] as usize) as isize;
+        self.parent_or_size[x] as usize
+    }
+
+    pub fn unite(&mut self, x: usize, y: usize) -> usize {
+        let (mut a, mut b) = (self.root(x), self.root(y));
+        if a == b {
+            return a;
+        }
+        if -self.parent_or_size[a] < -self.parent_or_size[b] {
+            std::mem::swap(&mut a, &mut b);
+        }
+        self.parent_or_size[a] += self.parent_or_size[b];
+        self.parent_or_size[b] = a as isize;
+        a
+    }
+}
 
 fn main() {
-    input!{
-        mut T: usize,
+    input! {
+        N: usize,
+        Q: usize,
+        LR: [[usize; 2]; Q],
     }
-    while T > 0 {
-        input! {
-            A: usize,
-            S: usize,
-        }
-        let mut is_ok = false;
-        if 2 * A <= S {
-            let d = S - 2 * A;
-            if d & A == 0 { is_ok = true; }
-        }
-        if is_ok {
-            println!("Yes");
-        } else {
-            println!("No")
-        }
-        T -= 1;
+    let mut tree = UnionFind::new(N+1);
+    for lr in LR.iter() {
+        tree.unite(lr[0]-1, lr[1]);
     }
+    let ans = if tree.is_same(0, N) { "Yes" } else { "No" };
+    println!("{}",ans);
 }
