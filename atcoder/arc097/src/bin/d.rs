0b2a95444b0fa92883e72e6ee2a5983197bd5ad8
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-05-02 16:30:24 +0900

    2-4-3 done

--- atcoder/arc097/src/bin/d.rs
+++ atcoder/arc097/src/bin/d.rs
@@ -1,5 +1,64 @@
+use std::collections::BTreeSet;
+#[allow(unused_imports)]
+use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap}, cmp::Reverse, cmp::{max, min}};
+#[allow(unused_imports)]
 use proconio::input;
+use proconio::marker::{Usize1, Isize1};
+
+// reference: https://github.com/rust-lang-ja/ac-library-rs/blob/master/src/dsu.rs
+struct UnionFind {
+    node: usize,
+    parent_or_size: Vec<isize>,
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> Self {
+        Self {
+            node: size,
+            parent_or_size: vec![-1; size]
+        }
+    }
+
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    pub fn root(&mut self, x: usize) -> usize {
+        if self.parent_or_size[x] < 0 {
+            return x;
+        }
+        self.parent_or_size[x] = self.root(self.parent_or_size[x] as usize) as isize;
+        self.parent_or_size[x] as usize
+    }
+
+    pub fn unite(&mut self, x: usize, y: usize) -> usize {
+        let (mut a, mut b) = (self.root(x), self.root(y));
+        if a == b {
+            return a;
+        }
+        if -self.parent_or_size[a] < -self.parent_or_size[b] {
+            std::mem::swap(&mut a, &mut b);
+        }
+        self.parent_or_size[a] += self.parent_or_size[b];
+        self.parent_or_size[b] = a as isize;
+        a
+    }
+}
 
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        M: usize,
+        P: [Usize1; N],
+        XY: [(Usize1, Usize1); M],
+    }
+    let mut uf = UnionFind::new(N);
+    for (x,y) in XY.into_iter() {
+        uf.unite(x,y);
+    }
+    let mut ans = 0;
+    for i in 0..N{
+        if uf.is_same(P[i], i) { ans += 1; }
+    }
+    println!("{}",ans);
 }
