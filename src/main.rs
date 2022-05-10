34143dd8a528bdcc8dde8b9740d4c063dd9a3fbc
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-05-10 21:48:41 +0900

    reset main

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,4 @@ use std::{collections::{BinaryHeap, HashMap, VecDeque, BTreeMap, BTreeSet}, cmp:
 use proconio::{input, marker::{Chars,Usize1, Isize1}};
 #[allow(non_snake_case)]
 fn main() {
-    input! {
-        N:usize,
-    }
-    let mut ans = 0;
-    let sq = (N as f64).sqrt() as usize;
-    for i in 1..=sq {
-        ans += N / i - i;
-    }
-    ans*=2;
-    for i in 1..=sq { ans += 1;}
-    println!("{}",ans);
 }
