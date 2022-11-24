923abd851e3227d04795f8c8ab2d834ff2c34e3f
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-11-25 03:12:54 +0900

    retry 2-1-3 5 with 0-1BFS

--- antBookPractice/2-1-3/5.rs
+++ antBookPractice/2-1-3/5.rs
@@ -60,3 +60,76 @@ fn main() {
     }
     println!("NO");
 }
+
+// 以下、0-1BFSを使用したもの。
+// わかり易い記事:https://betrue12.hateblo.jp/entry/2018/12/08/000020
+/* https://atcoder.jp/contests/arc005/tasks/arc005_3
+
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, VecDeque},
+};
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = 10_000_000;
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        H: usize,
+        W: usize,
+        grid: [Chars; H]
+    }
+    let mut deque: VecDeque<(usize, usize)> = VecDeque::new();
+    let mut dist = vec![vec![FIRST_VALUE; W]; H];
+    let mut goal = (FIRST_VALUE, FIRST_VALUE);
+    for h in 0..H {
+        for w in 0..W {
+            if grid[h][w] == 's' {
+                deque.push_back((h, w));
+                dist[h][w] = 0;
+            } else if grid[h][w] == 'g' {
+                goal = (h, w);
+            }
+        }
+    }
+    while !deque.is_empty() {
+        let (x, y) = deque.pop_front().unwrap();
+        for dxy in DXY.iter() {
+            let nx = x as isize + dxy.0;
+            let ny = y as isize + dxy.1;
+            if !(0 <= nx && nx < H as isize) || !(0 <= ny && ny < W as isize) {
+                continue;
+            }
+            let nx = nx as usize;
+            let ny = ny as usize;
+            if grid[nx][ny] == '#' {
+                if dist[nx][ny] <= dist[x][y] + 1 {
+                    continue;
+                }
+                dist[nx][ny] = dist[x][y] + 1;
+                deque.push_back((nx, ny));
+            } else {
+                if dist[nx][ny] <= dist[x][y] {
+                    continue;
+                }
+                dist[nx][ny] = dist[x][y];
+                deque.push_front((nx, ny));
+            }
+        }
+    }
+    println!(
+        "{}",
+        if dist[goal.0][goal.1] <= 2 {
+            "YES"
+        } else {
+            "NO"
+        }
+    )
+}
+*/
