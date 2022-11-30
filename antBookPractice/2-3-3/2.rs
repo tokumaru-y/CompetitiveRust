337652e314898d9ee2402671cf7100ea4200cade
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-11-30 14:31:25 +0900

    2-3-3 2 done

--- antBookPractice/2-3-3/2.rs
+++ antBookPractice/2-3-3/2.rs
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc153/tasks/abc153_e
 #[allow(unused_imports)]
 use itertools::Itertools;
 #[allow(unused_imports)]
@@ -16,4 +17,21 @@ const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 const FIRST_VALUE: usize = 1_000_000_000;
 const MOD: usize = 1_000_000_007;
 #[allow(non_snake_case)]
-fn main() {}
+fn main() {
+    input! {
+        H: usize,
+        N: usize,
+        AB: [(usize, usize); N]
+    }
+    let mut dp = vec![FIRST_VALUE; H + 1];
+    dp[0] = 0;
+    for (a, b) in AB.iter() {
+        for i in 0..H {
+            if dp[i] == FIRST_VALUE {
+                continue;
+            }
+            dp[min(a + i, H)] = min(dp[min(a + i, H)], dp[i] + b)
+        }
+    }
+    println!("{}", dp[H]);
+}
