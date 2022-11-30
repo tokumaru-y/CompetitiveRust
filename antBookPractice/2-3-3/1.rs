a8befcf8147b36c306785555101295d3c3d4cad1
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-11-30 14:23:26 +0900

    2-3-3 1 done

---
+++ antBookPractice/2-3-3/1.rs
@@ -0,0 +1,89 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C&lang=jp
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, VecDeque},
+    process::exit,
+};
+#[warn(dead_code)]
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = 10_000_000;
+const MOD: usize = 1_000_000_007;
+#[allow(non_snake_case)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+// input!マクロ　複数個所には記述しない
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        W: usize,
+        VW: [[usize; 2]; N]
+    }
+    let mut dp = vec![-1; W + 1];
+    dp[0] = 0;
+    for item in VW.iter() {
+        let v = item[0] as isize;
+        let w = item[1] as isize;
+        for i in 0..W {
+            if dp[i] == -1 {
+                continue;
+            }
+            if w as usize + i <= W {
+                dp[w as usize + i] = max(dp[w as usize + i], dp[i] + v);
+            }
+        }
+    }
+    println!("{}", dp.iter().max().unwrap());
+}
