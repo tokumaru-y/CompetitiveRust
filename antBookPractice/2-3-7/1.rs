80551bccb68f416dc35067d25ec3bb8ae924c498
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-03 17:04:44 +0900

    2-3-7 1 done

---
+++ antBookPractice/2-3-7/1.rs
@@ -0,0 +1,93 @@
+// https://yukicoder.me/problems/no/269
+#[allow(non_snake_case)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+// input!マクロ　複数個所には記述しない
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, VecDeque},
+    process::exit,
+};
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = 1_000_000_000;
+const MOD: usize = 1_000_000_007;
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        S: isize,
+        K: usize,
+    }
+    let S = S - (K * N * (N - 1) / 2) as isize;
+    if S < 0 {
+        println!("0");
+        exit(0);
+    }
+
+    let mut dp = vec![vec![0; S as usize + 1]; N + 1];
+    // dp[i][j] := jをi分割するときの総数
+    dp[0][0] = 1;
+    for i in (1..=N) {
+        for j in (0..=(S as usize)) {
+            if j >= i {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - i];
+                dp[i][j] %= MOD;
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    println!("{}", dp[N][S as usize]);
+}
--- antBookPractice/REVIEW_PROBLEMS.md
+++ antBookPractice/REVIEW_PROBLEMS.md
@@ -1,2 +1,4 @@
 ## 2022/11/30
 https://atcoder.jp/contests/arc057/tasks/arc057_b
+
+https://yukicoder.me/submissions/822245
