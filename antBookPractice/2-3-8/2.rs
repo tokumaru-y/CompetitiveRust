42894fcb4c21370c5249805e067fa839f2fef0da
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-03 22:51:49 +0900

    2-3-8 2 done

---
+++ antBookPractice/2-3-8/2.rs
@@ -0,0 +1,67 @@
+// https://atcoder.jp/contests/abc021/tasks/abc021_d
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, VecDeque},
+    process::exit,
+};
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = 1_000_000_000;
+const MOD: usize = 1_000_000_007;
+// 二項係数
+// 参考: https://drken1215.hatenablog.com/entry/2018/06/08/210000
+
+fn combination_init(mod_num: &usize) -> (Vec<usize>, Vec<usize>) {
+    let max_len = 510000;
+    // 累積積
+    let mut fac = vec![0; max_len];
+    // 累積積（除算）
+    let mut finv = vec![0; max_len];
+    // 逆元
+    let mut inv = vec![0; max_len];
+
+    fac[0] = 1;
+    fac[1] = 1;
+    finv[0] = 1;
+    finv[1] = 1;
+    inv[1] = 1;
+    for i in 2..max_len {
+        fac[i] = fac[i - 1] * i % mod_num;
+        inv[i] = mod_num - inv[mod_num % i] * (mod_num / i) % mod_num;
+        finv[i] = finv[i - 1] * inv[i] % mod_num;
+    }
+    (fac, finv)
+}
+
+fn combination_calc(
+    n: usize,
+    k: usize,
+    mod_num: &usize,
+    fac: &Vec<usize>,
+    finv: &Vec<usize>,
+) -> usize {
+    if n < k {
+        return 0;
+    }
+    if n < 0 || k < 0 {
+        return 0;
+    }
+    fac[n] * (finv[k] * finv[n - k] % mod_num) % mod_num
+}
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        K: usize,
+    }
+    let (fac, finv) = combination_init(&MOD);
+    println!("{}", combination_calc(N + K - 1, K, &MOD, &fac, &finv))
+}
--- antBookPractice/REVIEW_PROBLEMS.md
+++ antBookPractice/REVIEW_PROBLEMS.md
@@ -1,4 +1,5 @@
 ## 2022/11/30
 https://atcoder.jp/contests/arc057/tasks/arc057_b
-
+# 2022/12/
 https://yukicoder.me/submissions/822245
+https://atcoder.jp/contests/abc021/tasks/abc021_d
