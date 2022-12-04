8e0f9bb60a00f1f6d6249b78a6518415e026f025
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-04 13:48:55 +0900

    update lib unionfind

--- src/lib/unionfind.rs
+++ src/lib/unionfind.rs
@@ -8,7 +8,9 @@ impl UnionFind {
     pub fn new(size: usize) -> Self {
         Self {
             node: size,
-            parent_or_size: vec![-1; size]
+            // parent_or_size[x] < 0:x is parent
+            // parent_or_size[x] > 0:x's parent is parent_or_size[x]
+            parent_or_size: vec![-1; size],
         }
     }
 
@@ -35,5 +37,28 @@ impl UnionFind {
         self.parent_or_size[a] += self.parent_or_size[b];
         self.parent_or_size[b] = a as isize;
         a
+    }
+
+    pub fn size(&mut self, x: usize) -> usize {
+        let a = self.root(x);
+        -self.parent_or_size[a] as usize
+    }
+
+    pub fn groups(&mut self) -> Vec<Vec<usize>> {
+        let mut roots = vec![0; self.node];
+        let mut group_size = vec![0; self.node];
+        for i in 0..self.node {
+            roots[i] = self.root(i)
+            group_size[roots[i]] += 1;
+        }
+        let mut res = vec![Vec::new(), self.node];
+        for i in 0..self.node {
+            res[i].reserve(group_size[i]);
+        }
+        for i in 0..self.node {
+            res[roots[i]].push(i);
+        }
+
+        res.into_iter().filter(|x| !x.is_empty()).collect::<Vec<Vec<usize>>>()
     }
 }
