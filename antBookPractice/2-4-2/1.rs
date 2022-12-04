cf9bb3f3bc9599f9bad0b6f4cffe6ed41b7fa860
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-04 12:58:49 +0900

    2-4-2 1 done

---
+++ antBookPractice/2-4-2/1.rs
@@ -0,0 +1,30 @@
+// https://atcoder.jp/contests/abc085/tasks/abc085_b
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = 1_000_000_000;
+const MOD: usize = 1_000_000_007;
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        D: [usize; N]
+    }
+    let mut set = HashSet::new();
+    for d in D.into_iter() {
+        set.insert(d);
+    }
+    println!("{}", set.len());
+}
