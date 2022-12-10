f64177b434aea6a1ac1585e9ed586e6f8d230238
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-10 14:40:25 +0900

    2-4-3 3 done

--- antBookPractice/2-4-3/3.rs
+++ antBookPractice/2-4-3/3.rs
@@ -1,1 +1,113 @@
 // https://atcoder.jp/contests/arc097/tasks/arc097_b
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = 1_000_000_000;
+const MOD: usize = 1_000_000_007;
+// reference: https://github.com/rust-lang-ja/ac-library-rs/blob/master/src/dsu.rs
+struct UnionFind {
+    node: usize,
+    parent_or_size: Vec<isize>,
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> Self {
+        Self {
+            node: size,
+            // parent_or_size[x] < 0:x is parent
+            // parent_or_size[x] > 0:x's parent is parent_or_size[x]
+            parent_or_size: vec![-1; size],
+        }
+    }
+
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    pub fn root(&mut self, x: usize) -> usize {
+        if self.parent_or_size[x] < 0 {
+            return x;
+        }
+        self.parent_or_size[x] = self.root(self.parent_or_size[x] as usize) as isize;
+        self.parent_or_size[x] as usize
+    }
+
+    pub fn unite(&mut self, x: usize, y: usize) -> usize {
+        let (mut a, mut b) = (self.root(x), self.root(y));
+        if a == b {
+            return a;
+        }
+        if -self.parent_or_size[a] < -self.parent_or_size[b] {
+            std::mem::swap(&mut a, &mut b);
+        }
+        self.parent_or_size[a] += self.parent_or_size[b];
+        self.parent_or_size[b] = a as isize;
+        a
+    }
+
+    pub fn size(&mut self, x: usize) -> usize {
+        let a = self.root(x);
+        -self.parent_or_size[a] as usize
+    }
+
+    pub fn groups(&mut self) -> Vec<Vec<usize>> {
+        let mut roots = vec![0; self.node];
+        let mut group_size = vec![0; self.node];
+        for i in 0..self.node {
+            roots[i] = self.root(i);
+            group_size[roots[i]] += 1;
+        }
+        let mut res = vec![Vec::new(); self.node];
+        for i in 0..self.node {
+            res[i].reserve(group_size[i]);
+        }
+        for i in 0..self.node {
+            res[roots[i]].push(i);
+        }
+
+        res.into_iter()
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<Vec<usize>>>()
+    }
+}
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        M: usize,
+        P: [usize; N],
+        XY: [(Usize1, Usize1); M],
+    }
+    let mut index_list = vec![0; N];
+    let mut uf = UnionFind::new(N);
+    for (x, y) in XY.into_iter() {
+        uf.unite(x, y);
+    }
+
+    for i in 0..N {
+        index_list[P[i] - 1] = i;
+    }
+
+    let mut ans = 0;
+    for i in 0..N {
+        let ind = index_list[i];
+        if uf.is_same(ind, i) || P[i] == i + 1 {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
+}
