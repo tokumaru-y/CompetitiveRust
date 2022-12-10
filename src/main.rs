52944b6ab110be0d103f2bfdfde5bfef44aad4e2
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-10 18:39:28 +0900

    rewrite main

--- src/main.rs
+++ src/main.rs
@@ -15,41 +15,5 @@ use std::{
 const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 const FIRST_VALUE: usize = 1_000_000_000;
 const MOD: usize = 1_000_000_007;
-
-fn dfs(x: usize, dist: &mut Vec<isize>, colors: &mut Vec<isize>, graph: &Vec<Vec<(usize, usize)>>) {
-    for (nx, d) in graph[x].iter() {
-        if colors[*nx] != -1 {
-            continue;
-        }
-        dist[*nx] = *d as isize;
-        colors[*nx] = if dist[*nx] % 2 == 0 {
-            colors[x]
-        } else {
-            (colors[x] + 1) % 2
-        };
-        dfs(*nx, dist, colors, graph);
-    }
-}
-
 #[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: usize,
-        UVW: [(Usize1, Usize1, usize); N-1]
-    }
-    let mut dist = vec![0; N];
-    let mut colors = vec![-1; N];
-    let mut graph: Vec<Vec<(usize, usize)>> = vec![vec![]; N];
-
-    for (u, v, w) in UVW.into_iter() {
-        graph[u].push((v, w));
-        graph[v].push((u, w));
-    }
-
-    colors[0] = 0;
-    dfs(0, &mut dist, &mut colors, &graph);
-
-    for c in colors.into_iter() {
-        println!("{}", c);
-    }
-}
+fn main() {}
