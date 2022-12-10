03c8065aed9be1c325e242c04e831eaae6a0c0f5
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-11 01:11:58 +0900

    2-5-3 5 done

--- src/lib/dijkstra.rs
+++ src/lib/dijkstra.rs
@@ -1,21 +1,16 @@
-fn dijkstra(
-    x: usize,
-    graph: &Vec<Vec<(usize, isize, isize)>>,
-    cost_graph: &mut Vec<isize>,
-    is_yen: bool,
-) {
+fn dijkstra(x: usize, graph: &Vec<Vec<(usize, isize)>>, cost_graph: &mut Vec<isize>) {
     let mut pq = BinaryHeap::new();
     cost_graph[x] = 0;
     pq.push((0, x));
 
     while !pq.is_empty() {
         let (_, nx) = pq.pop().unwrap();
-        for (y, yen, snuuk) in graph[nx].iter() {
-            let c = if is_yen { *yen } else { *snuuk };
-            if cost_graph[*y] <= cost_graph[nx] - c {
+        for (y, cost) in graph[nx].iter() {
+            let c = *cost;
+            if cost_graph[*y] <= cost_graph[nx] + c {
                 continue;
             }
-            cost_graph[*y] = cost_graph[nx] - c;
+            cost_graph[*y] = cost_graph[nx] + c;
             pq.push((-cost_graph[*y], *y));
         }
     }
