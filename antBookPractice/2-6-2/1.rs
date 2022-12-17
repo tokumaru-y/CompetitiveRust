d28a58d1eb4c160290f9ae6163c37cc095a7eaac
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2022-12-17 19:58:37 +0900

    2-6-2 1 done

---
+++ antBookPractice/2-6-2/1.rs
@@ -0,0 +1,87 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_E
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, VecDeque},
+    process::exit,
+};
+#[warn(dead_code)]
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = 10_000_000;
+const MOD: usize = 1_000_000_007;
+#[allow(non_snake_case)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+// input!マクロ　複数個所には記述しない
+fn ext_gcd(a: isize, b: isize, x: &mut isize, y: &mut isize) -> isize {
+    if b == 0 {
+        *x = 1;
+        *y = 0;
+        return a;
+    }
+    let d = ext_gcd(b, a % b, y, x);
+    *y -= a / b * (*x);
+    d
+}
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        A: isize,
+        B: isize,
+    }
+    let mut x = 0;
+    let mut y = 0;
+    let d = ext_gcd(A, B, &mut x, &mut y);
+    println!("{} {}", x, y);
+}
