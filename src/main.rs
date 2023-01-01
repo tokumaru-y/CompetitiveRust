f85637c6b33d152c36f82ea2da46a544bb382713
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-01-01 14:20:13 +0900

    modify to init

--- src/main.rs
+++ src/main.rs
@@ -24,25 +24,4 @@ const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 const FIRST_VALUE: usize = 1_000_000_000;
 const MOD: usize = 1_000_000_007;
 #[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: usize,
-        K: usize,
-        D: usize,
-        A: [usize; N]
-    }
-    let mut dp = vec![vec![-1; D]; K + 1];
-    dp[0][0] = 0;
-    for i in (0..N) {
-        for j in (0..K).rev() {
-            for k in (0..D) {
-                if dp[j][k] >= 0 {
-                    dp[j + 1][(k + A[i]) % D] =
-                        max(dp[j + 1][(k + A[i]) % D], dp[j][k] + A[i] as isize);
-                }
-            }
-        }
-    }
-
-    println!("{}", dp[K][0]);
-}
+fn main() {}
