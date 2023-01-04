f05ee6227338358aebd4594dd8adc566029c699b
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-01-04 13:47:19 +0900

    reset main

--- src/main.rs
+++ src/main.rs
@@ -24,25 +24,4 @@ const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 const FIRST_VALUE: usize = 1_000_000_000;
 const MOD: usize = 1_000_000_007;
 #[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: usize,
-        M: usize,
-    }
-    let mut ans = (0, 0);
-    for i in (1..=9) {
-        let mut div = 0;
-        for j in (1..=N) {
-            div = (10 * div + i) % M;
-            if div == 0 {
-                ans = max(ans, (j, i));
-            }
-        }
-    }
-
-    if ans == (0, 0) {
-        println!("-1");
-    } else {
-        println!("{}", vec![ans.1 as usize; ans.0 as usize].iter().join(""))
-    }
-}
+fn main() {}
