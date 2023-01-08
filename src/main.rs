68a8e0524b9a7cb4282be9b91fe303f344d3f853
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-01-08 13:20:25 +0900

    reflush main

--- src/main.rs
+++ src/main.rs
@@ -34,41 +34,4 @@ const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 const FIRST_VALUE: isize = std::isize::MIN;
 const MOD: usize = 1_000_000_007;
 #[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: usize,
-        TXA: [(usize, usize, isize); N]
-    }
-    let ll = 10usize.pow(5);
-    let mut dp = vec![vec![FIRST_VALUE; 5]; ll + 1];
-    dp[0][0] = 0;
-    let mut time_table = vec![vec![]; ll + 1];
-
-    for (t, x, a) in TXA.into_iter() {
-        time_table[t].push((x, a));
-    }
-
-    for i in 0..(ll) {
-        for j in 0..5 {
-            if dp[i][j] == FIRST_VALUE {
-                continue;
-            }
-            for (x, a) in time_table[i + 1].iter() {
-                if !(*x == j + 1 || *x + 1 == j || *x == j) {
-                    continue;
-                }
-                dp[i + 1][*x] = max(dp[i + 1][*x], dp[i][j] + *a);
-            }
-
-            for d in [-1, 0, 1].into_iter() {
-                let nd = j as isize + *d;
-                if !(0 <= nd && nd <= 4) {
-                    continue;
-                }
-                let nd = nd as usize;
-                dp[i + 1][nd] = max(dp[i + 1][nd], dp[i][j]);
-            }
-        }
-    }
-    println!("{}", dp[ll].iter().max().unwrap());
-}
+fn main() {}
