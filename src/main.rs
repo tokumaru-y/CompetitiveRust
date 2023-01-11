7b5a7d538ef3e71b1cecb519d10322d2f1a62a7d
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-01-11 23:24:35 +0900

    reset main

--- src/main.rs
+++ src/main.rs
@@ -35,46 +35,4 @@ const FIRST_VALUE: usize = std::usize::MAX;
 const MOD: usize = 1_000_000_007;
 
 #[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: usize,
-        K: usize,
-        P: [usize; N]
-    }
-    let mut under = vec![0; N + 1];
-    let mut pile = vec![0; N + 1];
-    let mut btree: BTreeSet<usize> = BTreeSet::new();
-    let mut ans = vec![-1; N + 1];
-
-    for i in 0..N {
-        let mut range = btree.range(P[i]..);
-        let value = if let Some(x) = range.next() {
-            Some(*x)
-        } else {
-            None
-        };
-        if value.is_some() {
-            let x: usize = value.unwrap();
-            under[P[i]] = x;
-            pile[P[i]] = pile[x] + 1;
-            btree.remove(&x);
-            btree.insert(P[i]);
-        } else {
-            pile[P[i]] = 1;
-            btree.insert(P[i]);
-        }
-
-        if pile[P[i]] == K {
-            btree.remove(&P[i]);
-            let mut n = P[i];
-            for _ in 0..K {
-                ans[n] = (i + 1) as isize;
-                n = under[n];
-            }
-        }
-    }
-
-    for i in 1..=N {
-        println!("{}", ans[i]);
-    }
-}
+fn main() {}
