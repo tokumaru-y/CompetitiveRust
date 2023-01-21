1409a5306d4849739b7f52ffe0e93010d1b9226c
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-01-21 14:31:00 +0900

    educational_dp k done

---
+++ educational_dp/k.rs
@@ -0,0 +1,54 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+use num_traits::Pow;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        K: usize,
+        A: [usize; N]
+    }
+    let mut dp = vec![false; 10usize.pow(5) + 10];
+    for i in 0..=K {
+        for a in 0..N {
+            if i + A[a] <= (10usize.pow(5)) {
+                dp[i + A[a]] |= !dp[i];
+            }
+        }
+    }
+    println!("{}", if dp[K] { "First" } else { "Second" });
+}
