acf3495f9ee5f3a29ccb81c6593762b9c434f248
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-01-28 20:50:01 +0900

    arc120 b done

--- atcoder/REVIEW_PROBLEMS.md
+++ atcoder/REVIEW_PROBLEMS.md
@@ -30,3 +30,4 @@ https://atcoder.jp/contests/abc217/tasks/abc217_e
 https://atcoder.jp/contests/abc208/tasks/abc208_d
 https://atcoder.jp/contests/abc204/tasks/abc204_d
 https://atcoder.jp/contests/arc121/tasks/arc121_b
+https://atcoder.jp/contests/arc120/tasks/arc120_b
--- atcoder/arc120/src/bin/b.rs
+++ atcoder/arc120/src/bin/b.rs
@@ -1,5 +1,73 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 998244353;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        H: usize,
+        W: usize,
+        S: [Chars; H]
+    }
+    let mut ans = 1;
+    let mut red_table = vec![0; 500 * 500 + 1];
+    let mut blue_table = vec![0; 500 * 500 + 1];
+    let mut any_table = vec![0; 500 * 500 + 1];
+
+    for i in 0..H {
+        for j in 0..W {
+            if S[i][j] == 'R' {
+                red_table[i + j] += 1;
+            } else if S[i][j] == 'B' {
+                blue_table[i + j] += 1;
+            } else {
+                any_table[i + j] += 1;
+            }
+        }
+    }
+
+    for i in 0..(500 * 500 + 1) {
+        if red_table[i] > 0 && blue_table[i] > 0 {
+            ans = 0;
+            break;
+        }
+        if red_table[i] + blue_table[i] == 0 && any_table[i] > 0 {
+            ans *= 2;
+        }
+        ans %= MOD;
+    }
+
+    println!("{}", ans);
 }
