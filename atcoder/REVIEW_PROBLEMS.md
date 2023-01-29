070d36fdf30e3cd5029ee9766d7475505a535ddd
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-01-29 16:35:25 +0900

    arc118 b done

--- atcoder/REVIEW_PROBLEMS.md
+++ atcoder/REVIEW_PROBLEMS.md
@@ -33,3 +33,4 @@ https://atcoder.jp/contests/arc121/tasks/arc121_b
 https://atcoder.jp/contests/arc120/tasks/arc120_b
 https://atcoder.jp/contests/abc202/tasks/abc202_d
 https://atcoder.jp/contests/arc119/tasks/arc119_b
+https://atcoder.jp/contests/arc118/tasks/arc118_b
--- atcoder/arc118/src/bin/b.rs
+++ atcoder/arc118/src/bin/b.rs
@@ -1,5 +1,63 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        K: usize,
+        N: isize,
+        M: isize,
+        A: [isize; K]
+    }
+
+    let mut b = vec![];
+    for i in 0..K {
+        b.push(M * A[i] / N);
+    }
+
+    let mut diff = vec![];
+    for i in 0..K {
+        diff.push((N * b[i] - M * A[i], i));
+    }
+
+    diff.sort();
+    let mut left_add = M - b.iter().sum::<isize>();
+    for i in 0..left_add {
+        b[diff[i as usize].1] += 1;
+    }
+
+    println!("{}", b.into_iter().join(" "));
 }
