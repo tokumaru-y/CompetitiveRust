50a6254df6427730c6d2ccd2f9d9dc094fb6029e
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-02-09 20:47:47 +0900

    sumitrust2019 e done

---
+++ atcoder/sumitrust2019/src/bin/e.rs
@@ -0,0 +1,58 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        A: [usize; N]
+    }
+    let mut map: Vec<VecDeque<usize>> = vec![VecDeque::new(); N + 1];
+    for _ in 0..3 {
+        map[0].push_back(1);
+    }
+    let mut ans = 1;
+    for i in 0..N {
+        let a = A[i];
+        let mut v: &mut VecDeque<usize> = &mut map[a];
+        ans *= v.len();
+        ans %= MOD;
+        v.pop_back();
+        map[a + 1].push_back(1);
+    }
+
+    println!("{}", ans);
+}
