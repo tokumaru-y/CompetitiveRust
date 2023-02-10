3cb958bc9b75b54ec3096e8c3c8953038a43d8bd
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-02-11 01:01:30 +0900

    3-2-1 3 done

---
+++ antBookPractice/3-2-1/3.rs
@@ -0,0 +1,56 @@
+//https://atcoder.jp/contests/abc038/tasks/abc038_c
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        mut A: [usize; N]
+    }
+    let mut ans = 0;
+    let mut right = 1;
+    for left in 0..N {
+        if right == left {
+            right = left + 1;
+        }
+        while right < N && A[right - 1] < A[right] {
+            right += 1;
+        }
+        ans += right - left;
+    }
+    println!("{}", ans);
+}
