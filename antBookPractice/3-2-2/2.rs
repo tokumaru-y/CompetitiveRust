bc30c5c21f393e2ac53437757a8ef366035511bf
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-02-12 12:07:07 +0900

    3-2-2 2 done

--- antBookPractice/3-2-2/2.rs
+++ antBookPractice/3-2-2/2.rs
@@ -1,4 +1,3 @@
-// https://atcoder.jp/contests/abc017/tasks/abc017_4
 #[allow(unused_imports)]
 use itertools::Itertools;
 #[allow(unused_imports)]
@@ -6,13 +5,17 @@ use proconio::{
     input,
     marker::{Chars, Isize1, Usize1},
 };
-use std::fmt::Debug;
 #[allow(unused_imports)]
 use std::{
     cmp::Reverse,
     cmp::{max, min},
     collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
     process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
 };
 fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
     if x.is_ok() {
@@ -21,9 +24,16 @@ fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
         x.unwrap_err()
     }
 }
+
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
 const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
-const FIRST_VALUE: usize = 1_000_000_000;
+const FIRST_VALUE: usize = std::usize::MAX;
 const MOD: usize = 1_000_000_007;
+
 #[allow(non_snake_case)]
 fn main() {
     input! {
@@ -31,29 +41,24 @@ fn main() {
         M: usize,
         F: [usize; N]
     }
-    let mut cnt_table = vec![0; M + 1];
-    // 各rightに対する適用可能な最左端のindex
-    let mut left_index_list = vec![0; N + 1];
-
-    let mut left = 0;
-    for right in (0..N) {
-        cnt_table[F[right]] += 1;
-
-        while left < right && cnt_table[F[right]] > 1 {
-            cnt_table[F[left]] -= 1;
-            left += 1;
-        }
-
-        left_index_list[right + 1] = left;
-    }
-
     let mut dp = vec![0; N + 1];
     let mut sum = vec![0; N + 2];
+    let mut left_index = vec![0; N + 1];
 
-    dp[0] = 1;
+    let mut left = 0;
+    let mut fnum = vec![0; M + 1];
+    for right in 0..N {
+        fnum[F[right]] += 1;
+        while left < right && fnum[F[right]] > 1 {
+            fnum[F[left]] -= 1;
+            left += 1;
+        }
+        left_index[right + 1] = left;
+    }
+
     sum[1] = 1;
-    for i in (1..=N) {
-        dp[i] = (sum[i] - sum[left_index_list[i]] + MOD) % MOD;
+    for i in 1..=N {
+        dp[i] = (sum[i] - sum[left_index[i]] + MOD) % MOD;
         sum[i + 1] = (sum[i] + dp[i]) % MOD;
     }
 
