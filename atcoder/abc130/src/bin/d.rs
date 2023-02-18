63b4b3662e3d946870cb406c4372e8a11598a59e
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-02-18 12:37:39 +0900

    abc130 d done

--- atcoder/abc130/src/bin/d.rs
+++ atcoder/abc130/src/bin/d.rs
@@ -1,5 +1,63 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        K: usize,
+        A: [usize; N]
+    }
+    let mut ans = 0;
+    let mut right = 0;
+    let mut sum = 0;
+    for left in 0..N {
+        while right < N && sum < K {
+            sum += A[right];
+            right += 1;
+        }
+        if sum >= K {
+            ans += N - (right) + 1;
+        }
+        if right == left {
+            right += 1;
+        } else {
+            sum -= A[left];
+        }
+    }
+
+    println!("{}", ans);
 }
