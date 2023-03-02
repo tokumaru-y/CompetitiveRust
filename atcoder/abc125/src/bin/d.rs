d85dbcb5519aff6e8484c817f40255525138f290
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-02 22:44:15 +0900

    abc125 d done

--- atcoder/abc125/src/bin/d.rs
+++ atcoder/abc125/src/bin/d.rs
@@ -1,5 +1,59 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [isize; N]
+    }
+    let mut sum = 0;
+    let mut min_num = std::isize::MAX;
+    let mut minus_cnt = 0;
+    for i in 0..N {
+        min_num = min(min_num, A[i].abs());
+        if A[i] <= 0 {
+            minus_cnt += 1;
+        }
+        sum += A[i].abs();
+    }
+
+    if minus_cnt % 2 == 0 {
+        println!("{}", sum);
+    } else {
+        println!("{}", sum - (min_num * 2));
+    }
 }
