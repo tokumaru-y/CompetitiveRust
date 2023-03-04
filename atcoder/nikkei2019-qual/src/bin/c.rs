559e3ae7b10a4b3d12fa3506fad0c01cc0884476
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-04 13:57:45 +0900

    nikkei2019-qual c done

---
+++ atcoder/nikkei2019-qual/src/bin/c.rs
@@ -0,0 +1,62 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        AB: [(isize, isize); N]
+    }
+    let mut tree = BinaryHeap::new();
+
+    for i in 0..N {
+        let (a, b) = AB[i];
+        tree.push((a + b, a, b));
+    }
+
+    let mut ans = 0isize;
+
+    for i in 0..N {
+        let (total, a, b) = tree.pop().unwrap();
+        if i % 2 == 0 {
+            ans += a;
+        } else {
+            ans -= b;
+        }
+    }
+
+    println!("{}", ans);
+}
