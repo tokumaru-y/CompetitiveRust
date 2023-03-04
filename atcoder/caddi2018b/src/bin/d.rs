4ce2662e77b8c60293826268c84ee039281be296
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-04 14:14:24 +0900

    caddi2018b d done

---
+++ atcoder/caddi2018b/src/bin/d.rs
@@ -0,0 +1,50 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+fn read() -> usize {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<usize>().unwrap()
+}
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        A: [usize; N]
+    }
+    let mut is_all_even = true;
+    for i in 0..N {
+        is_all_even &= (A[i] % 2) == 0;
+    }
+
+    let ans = if is_all_even { "second" } else { "first" };
+    println!("{}", ans);
+}
