9550440293eb71e6b6aff14f7d450c462ae2c217
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-15 23:26:59 +0900

    abc108 c done

--- atcoder/abc108/src/bin/c.rs
+++ atcoder/abc108/src/bin/c.rs
@@ -1,5 +1,51 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        K: usize,
+    }
+    let mut ans = 0usize;
+    let mut mod_cnt = vec![0; K + 1];
+    for i in 1..=N {
+        mod_cnt[i % K] += 1;
+    }
+
+    ans += mod_cnt[0] * mod_cnt[0] * mod_cnt[0];
+
+    if K % 2 == 0 {
+        ans += mod_cnt[K / 2] * mod_cnt[K / 2] * mod_cnt[K / 2];
+    }
+
+    println!("{}", ans);
 }
