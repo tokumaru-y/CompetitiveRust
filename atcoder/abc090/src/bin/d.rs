3dab8090c6f2731c1a46129be53121103f7f99da
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-17 18:22:52 +0900

    abc090 d done

--- atcoder/abc090/src/bin/d.rs
+++ atcoder/abc090/src/bin/d.rs
@@ -1,5 +1,58 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        K: usize,
+    }
+
+    if K == 0 {
+        println!("{}", N * N);
+        exit(0)
+    }
+    let mut ans = 0;
+    for b in 1..=N {
+        if b <= K {
+            continue;
+        }
+        ans += (b - K) * ((N + 1) / b);
+
+        let left = K + b * ((N + 1) / b);
+        let right = N;
+        if left <= right {
+            ans += right - left + 1;
+        }
+    }
+
+    println!("{}", ans);
 }
