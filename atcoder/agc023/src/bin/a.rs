8621b97b1f6fdf6b2fde6de5101410f632408bc2
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-17 17:23:59 +0900

    agc023 a done

---
+++ atcoder/agc023/src/bin/a.rs
@@ -0,0 +1,58 @@
+use proconio::input;
+
+fn main() {
+    todo!();
+}
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        A: [isize; N]
+    }
+    let mut sum_v = vec![0; N + 1];
+    let mut ans = 0;
+    for i in 0..N {
+        sum_v[i + 1] = sum_v[i] + A[i];
+    }
+    let mut table = HashMap::new();
+
+    for i in 0..=N {
+        if table.contains_key(&sum_v[i]) {
+            ans += table.get(&sum_v[i]).unwrap();
+        }
+        *table.entry(&sum_v[i]).or_insert(0) += 1;
+    }
+
+    println!("{}", ans);
+}
