2dcb1c19c638e3ec50663ce81c387a10ec356d45
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-22 01:06:48 +0900

    abc53 d done

--- atcoder/abc053/src/bin/d.rs
+++ atcoder/abc053/src/bin/d.rs
@@ -1,5 +1,53 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N]
+    }
+    let mut table: HashMap<usize, usize> = HashMap::new();
+    let mut total = 0;
+    for i in 0..N {
+        *table.entry(A[i]).or_default() += 1;
+    }
+    for (k, v) in table.iter() {
+        if *v >= 2 {
+            total += *v - 1;
+        }
+    }
+    let mut ans = N - total;
+    if total % 2 == 1 {
+        ans -= 1;
+    }
+    println!("{}", ans);
 }
