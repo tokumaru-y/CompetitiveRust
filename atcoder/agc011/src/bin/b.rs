78db2555ea113a0bf87bfbe318d24ae2e70fdc64
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-21 18:10:32 +0900

    agc011 b done

--- atcoder/agc011/src/bin/b.rs
+++ atcoder/agc011/src/bin/b.rs
@@ -1,5 +1,50 @@
-use proconio::input;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
 
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        mut A: [usize; N]
+    }
+    A.sort();
+    let mut left_idx = 0;
+    let mut sum = A[0];
+
+    for i in 1..N {
+        if sum * 2 < A[i] {
+            left_idx = i;
+        }
+        sum += A[i];
+    }
+
+    println!("{}", N - left_idx);
 }
