a38405aeaa27dfe63877108a3d87ad8e5c87cc32
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-24 00:03:45 +0900

    cf16-final

--- atcoder/REVIEW_PROBLEMS.md
+++ atcoder/REVIEW_PROBLEMS.md
@@ -72,3 +72,4 @@ https://atcoder.jp/contests/agc017/tasks/agc017_a
 https://atcoder.jp/contests/agc016/tasks/agc016_a
 https://atcoder.jp/contests/abc064/tasks/abc064_d
 https://atcoder.jp/contests/abc051/tasks/abc051_c
+https://atcoder.jp/contests/cf16-final/tasks/codefestival_2016_final_b
---
+++ atcoder/cf16-final/src/bin/b.rs
@@ -0,0 +1,48 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        mut N: usize,
+    }
+    for idx in 1..=N {
+        if idx * (idx + 1) >= 2 * N {
+            for j in 1..=idx {
+                if j == idx * (idx + 1) / 2 - N {
+                    continue;
+                }
+                println!("{}", j);
+            }
+            exit(0);
+        }
+    }
+}
