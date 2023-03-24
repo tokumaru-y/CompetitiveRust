932d4b1e3b36cd369d52a753bedae00e19eb3bde
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2023-03-25 00:54:35 +0900

    agc005 a done

---
+++ atcoder/agc005/src/bin/a.rs
@@ -0,0 +1,52 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    input,
+    marker::{Chars, Isize1, Usize1},
+};
+#[allow(unused_imports)]
+use std::{
+    cmp::Reverse,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    process::exit,
+};
+use std::{
+    fmt::Debug,
+    io::{stdout, Write},
+    mem::swap,
+};
+fn unwrap_result_type<T: Debug>(x: Result<T, T>) -> T {
+    if x.is_ok() {
+        x.unwrap()
+    } else {
+        x.unwrap_err()
+    }
+}
+
+const DXY: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const FIRST_VALUE: usize = std::usize::MAX;
+const MOD: usize = 1_000_000_007;
+
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        C: Chars
+    }
+    let mut ans = 0;
+    let mut cnt = 0isize;
+    for i in 0..(C.len()) {
+        if C[i] == 'S' {
+            cnt += 1;
+        } else {
+            cnt -= 1;
+            if cnt < 0 {
+                ans += 1;
+                cnt = 0;
+            }
+        }
+    }
+    ans += cnt;
+    println!("{}", ans);
+}
